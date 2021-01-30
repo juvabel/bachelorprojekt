129ac9d656198e5a3aff809d024f27afb5c0c7c3
Julia von Abel <julia.von-abel@uni.kn>
2021-01-30 12:16:55 +0100

    add Header and StartScreen

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="uk-width-1-1 header">
+   <h1 class="bankname uk-heading-small">Vabel Bank</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.bankname{
+    color: #ffffff;
+}
+
+.header{
+    background: #3E9DB1;
+}
+</style>
---
+++ src/components/StartScreen.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+   <h1 class="uk-heading-small uk-text-light uk-position-center">Please hold your hand over the interaction zone.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartScreen',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+</style>
